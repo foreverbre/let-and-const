34e3c74a8b2b740f73a9968db9ab93e1956b5b50
foreverbre <b.moore1861@gmail.com>
2021-09-01 19:43:58 -0700

    first commit

---
+++ let and const Exercise.js
@@ -0,0 +1,19 @@
+const PIE = 3.14;
+PIE = 42;
+
+/*What is the difference between var and let?
+var you can redeclare and reassign variables, var will hoist to the top of the 
+scope it is defined in, it is a function scope and you can access a hoisted variable
+let you cannot redeclare variables only reassign, creates a block-scoped variable
+
+What is the difference between var and const?
+var can reassign and redeclare variables and can access a hoisted variable 
+const is block-scoped and prevents a variable from being reassigned or redeclared
+
+What is the difference between let and const?
+let variables can only be reassigned 
+const prevents a variable from being reassigned or redclared
+
+What is hoisting?
+hoisting happens when variables are lifted to the top of the scope they are declared in.
+when using var, variable declarations are ran first before any initialization.*/
